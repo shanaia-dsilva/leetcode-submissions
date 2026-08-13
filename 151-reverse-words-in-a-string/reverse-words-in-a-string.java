d4376a50c696a7e123d8c4f8c4aa3ab69dc9f4fc
Shanaia D'Silva <shanaia.dsilva30@gmail.com>
2026-08-13 09:29:26 +0530

    Time: 15 ms (5.14%) | Memory: 46.9 MB (13.05%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,20 +1,20 @@
 class Solution {
     public String reverseWords(String s) {
-        // String rev[]=s.trim().split(' '), result="";
-        // for(int i=rev.length();i>=0; i++) {
-        //     result+=" "+rev[i].trim();
-        // }
-
-        String result="",word="";
-        s=s.trim()+" ";
-        for(int i=0;i<s.length();i++) {
-            char c=s.charAt(i);
-            word+=c;
-            if(c==' '){
-                result=word.trim()+" "+result.trim();
-                word="";
-            }     
+        String rev[]=s.trim().split(" "), result="";
+        for(int i=rev.length-1;i>=0; i--) {
+            result=result.trim()+" "+rev[i].trim();
         }
+
+        // String result="",word="";
+        // s=s.trim()+" ";
+        // for(int i=0;i<s.length();i++) {
+        //     char c=s.charAt(i);
+        //     word+=c;
+        //     if(c==' '){
+        //         result=word.trim()+" "+result.trim();
+        //         word="";
+        //     }     
+        // }
         return result.trim();
     }
 }
